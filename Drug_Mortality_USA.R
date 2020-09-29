d672aea4efa68915567414079dd5841205a6f831
A <gavin.k.armstrong@gmail.com>
2020-09-28 21:33:27 -0700

    Started mapping counties. Trying to decide colors.

--- Drug_Mortality_USA.R
+++ Drug_Mortality_USA.R
@@ -2,16 +2,17 @@ library(tidyverse)
 library(magrittr)
 library(readxl)
 library(viridis)
-#library(extrafont)
-
-#font_import()
 
 library(ggplot2)
 library(sf)
 library(maps)
 
 library(gganimate)
-#library(transformr)
+
+library(timeR)
+
+#library(extrafont)
+#font_import()
 
 rm(list = ls())
 
@@ -21,100 +22,90 @@ rm(list = ls())
 # Read data
 Drug <- read_csv(file="NCHS_-_Drug_Poisoning_Mortality_by_County__United_States.csv")
 
-# Void theme for maps
-#theme_set(theme_bw())
+# Use the Black and White as a base for maps
+theme_set(theme_bw())
 
 # Read USA multipolygons from the Maps package
-# USA <- st_as_sf(maps::map("usa", plot=FALSE, fill=TRUE)) %>%
-#   mutate(ID = str_to_title(ID))
+USA <- st_as_sf(maps::map("usa", plot=FALSE, fill=TRUE)) %>%
+  mutate(ID = str_to_title(ID))
 
-# Read State multipolygons from the Maps package
-States <- st_as_sf(maps::map("state", plot = FALSE, fill = TRUE)) %>%
-  mutate(ID = str_to_title(ID)) %>%
-  as_tibble() %>%
-  rename(State = ID)
+# # Read State multipolygons from the Maps package
+# States <- st_as_sf(maps::map("state", plot = FALSE, fill = TRUE)) %>%
+#   mutate(ID = str_to_title(ID)) %>%
+#   as_tibble() %>%
+#   rename(State = ID)
 
 # Read County multipolygons from the Maps package
-# Counties <- st_as_sf(maps::map("county", plot = FALSE, fill = TRUE)) %>%
-#   mutate(ID = as.character(ID)) %>%
-#   left_join(maps::county.fips, by=c("ID"="polyname")) %>%
-#   separate(col="ID", into=c("State", "County"), sep=",", extra="merge", fill="left") %>%
-#   mutate_at(vars(State, County), str_to_title) %>%
-#   as_tibble()
+Counties <- st_as_sf(maps::map("county", plot = FALSE, fill = TRUE)) %>%
+  mutate(ID = as.character(ID)) %>%
+  left_join(maps::county.fips, by=c("ID"="polyname")) %>%
+  separate(col="ID", into=c("State", "County"), sep=",", extra="merge", fill="left") %>%
+  mutate_at(vars(State, County), str_to_title) %>%
+  as_tibble()
 
 # Patch missing fips
-# Counties %<>% mutate(fips = if_else(State == "Florida" & County == "Okaloosa",
-#                                     as.integer(12091),
-#                                     fips),
-#                      fips = if_else(State == "Louisiana" & County == "St Martin",
-#                                     as.integer(22099),
-#                                     fips),
-#                      fips = if_else(State == "North Carolina" & County == "Currituck",
-#                                     as.integer(37053),
-#                                     fips),
-#                      fips = if_else(State == "Texas" & County == "Galveston",
-#                                     as.integer(48167),
-#                                     fips),
-#                      fips = if_else(State == "Virginia" & County == "Accomack",
-#                                     as.integer(51001),
-#                                     fips),
-#                      fips = if_else(State == "Washington" & County == "Pierce",
-#                                     as.integer(53053),
-#                                     fips),
-#                      fips = if_else(State == "Washington" & County == "San Juan",
-#                                     as.integer(53055),
-#                                     fips))
+Counties %<>% mutate(fips = if_else(State == "Florida" & County == "Okaloosa",
+                                    as.integer(12091),
+                                    fips),
+                     fips = if_else(State == "Louisiana" & County == "St Martin",
+                                    as.integer(22099),
+                                    fips),
+                     fips = if_else(State == "North Carolina" & County == "Currituck",
+                                    as.integer(37053),
+                                    fips),
+                     fips = if_else(State == "Texas" & County == "Galveston",
+                                    as.integer(48167),
+                                    fips),
+                     fips = if_else(State == "Virginia" & County == "Accomack",
+                                    as.integer(51001),
+                                    fips),
+                     fips = if_else(State == "Washington" & County == "Pierce",
+                                    as.integer(53053),
+                                    fips),
+                     fips = if_else(State == "Washington" & County == "San Juan",
+                                    as.integer(53055),
+                                    fips))
 
-# Join the data to Counties
-# Counties %<>% left_join(Drug %>%
-#                           select(-County, -State),
-#                         by=c("fips"="FIPS"))
-# 
-# Counties %<>%
-#   filter(Year %in% c(2017, 2018)) %>%
-#   mutate(Year = as.factor(Year)) %>%
-#   select(Year,
-#          geom,
-#          MBDR = `Model-based Death Rate`)
+# Join the drug data to Counties
+County_Drug <- Counties %>%
+  left_join(Drug %>%
+              select(-County, -State),
+            by=c("fips"="FIPS")) %>%
+  select(Year,
+         geom,
+         MBDR = `Model-based Death Rate`)
+
+County_Drug %<>% filter(Year %in% c(2018))
 
 
-Drug %<>% select(Year,
-                 State,
-                 MBDR = `Model-based Death Rate`) %>%
-  group_by(State, Year) %>%
-  mutate(MBDR = first(MBDR)) %>%
-  distinct() %>%
-  ungroup()
-
-States %<>% left_join(Drug) %>%
-  filter(Year %in% c(2003, 2018))
-
-# Simple plot of State boundaries
-anim <- ggplot(airquality, aes(Day, Temp)) +
-  geom_point(aes(colour = factor(Month))) +
-  transition_time(Day)
-
-First_Plot <- ggplot(data=States) +
-  geom_sf(lwd = 0,
+# Plotting the data
+First_Plot <- ggplot(data=USA) +
+  geom_sf(fill = "antiquewhite1",
+          lwd=0,
+          color="black") +
+  geom_sf(data=County_Drug,
           aes(geometry=geom,
-              fill = MBDR)) +
+              fill = MBDR),
+          lwd=0,
+          color=NA) +
   scale_fill_viridis() +
   theme(axis.ticks = element_blank(),
         axis.text = element_blank(),
         panel.grid.major = element_blank(),
-        legend.position = "none") +
+        panel.background = element_rect(fill = "#000f0e"),
+        plot.background = element_rect(fill = "#000f0e"),
+        legend.position = "none")
+
+
++
   transition_time(Year)
+
+Timer <- createTimer(precision = "ms")
+Timer$start("Event 1")
 
 anim_save("First_Anim.gif", First_Plot)
 
-plot(First_Plot)
-
-First_Anim <- First_Plot +
-  transition_states(Year,
-                    transition_length = 2,
-                    state_length = 1)
-
-anim_save("First_Anim.gif", First_Anim)
+Timer$stop("Event 1")
 
 # Simple plot of State boundaries
 # ggplot(USA) +
